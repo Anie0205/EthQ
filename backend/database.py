5d4b33e178767e5a9cf449fc4313e3ad879cda4b
Ananya Verma <110318580+Anie0205@users.noreply.github.com>
2025-11-12 10:15:20 +0530

    Add declarative_base to database.py
    Added declarative_base to SQLAlchemy setup.

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
@@ -8,6 +8,8 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()  # <<<<<< Add this
 
 def get_db():
     db = SessionLocal()
